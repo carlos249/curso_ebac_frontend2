6fc678daf85fceee111d1dd47cb3235c5dd4c837
carlos249 <68656084+carlos249@users.noreply.github.com>
2023-01-05 17:16:49 -0300

    alteração css e js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-//Adicionando um evento de envio ao formulário
-document.getElementById('myform').addEventListener('submit'), function(event){
-        //pare o envio do formulário
-        event.preventDefault();
+document.getElementById('myform').addEventListener('submit', function(event) {
+    //impedir que o formulário seja enviado
+    event.preventDefault();
 
-        //obtendo os valores dos campos A e B
-        var fieldA = document.getElementById('field-a').value;
-        var fieldB = document.getElementById('field-b').value;
+    //obter os valores dos campos A e B
+    var fieldA = document.getElementById('field-a').value;
+    var fieldB = document.getElementById('field-b').value;
 
-        //verifique se o valor do campo B é maior que o valor do campo A
-        if (fieldB > fieldA){
-            //exiba uma mensagem positiva
-            alert('formulário válido');
-        } else {
-            //exiba uma mesagem negativa
-            alert('Formulário inválido');
-        }
-}
+    //verifique se o valor do campo B é maior que o valor do campo A
+    if (fieldB > fieldA){
+        //exibir uma mensagem positiva
+        alert('O formulário é válido');
+    } else {
+        //exibir uma mensagem negativa
+        alert('Formulário inválido');
+    }
+});
+    
